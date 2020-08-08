787e0c1afc6a4bdef4548104c1c4b37536ebc1a3
LiZhonggen001 <57128762+LiZhonggen001@users.noreply.github.com>
2020-08-08 16:27:28 +0800

    Update ReUserPass.cs

--- StudentMag/ReUserPass.cs
+++ StudentMag/ReUserPass.cs
@@ -20,7 +20,7 @@ namespace StudentMag
 
         private void btSave_Click(object sender, EventArgs e)
         {
-            MySqlConnection connection = new MySqlConnection("Database=student;Data Source=127.0.0.1;User Id = root; Password = li20010309; pooling = false; CharSet = utf8; port = 3306");
+            MySqlConnection connection = new MySqlConnection("Database=student;Data Source=127.0.0.1;User Id = root; Password = li; pooling = false; CharSet = utf8; port = 3306");
             try
             {
                 connection.Open();
